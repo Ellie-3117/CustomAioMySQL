886ba680169efa4ec23cf6cf573f403db7457e56
Ellie-3117 <elysiabot301127@gmail.com>
2025-03-22 22:40:11 +0700

    Update new AioMysql

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-from CustomAioMySql import CustomAioMySql
+from AioMySql import Database
+import asyncio
+async def testhelp():
+    m=Database()
+    await m.help()
+#asyncio.run(testhelp())
+async def test_connected():
+    m=Database()
+    await m.connected(db='setup',file_name='config.cfg')
+asyncio.run(test_connected())
